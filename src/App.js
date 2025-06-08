58f913042dd526ab773d594be8bd8efd06d4431b
Carlos romero <soportecarlosromero@gmail.com>
2025-06-07 21:46:25 -0500

    search another func

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ const list = [
 function getTitle(title) {
   return title;
 }
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
 function List() {
   return (
@@ -51,10 +59,8 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
       <hr />
-
       <List />
     </div>
   );
