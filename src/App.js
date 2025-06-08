1075d524003f88bf01803438a7799765d28e1d95
Carlos romero <soportecarlosromero@gmail.com>
2025-06-07 21:43:34 -0500

    list items func

--- src/App.js
+++ src/App.js
@@ -27,6 +27,26 @@ const list = [
 function getTitle(title) {
   return title;
 }
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -35,20 +55,7 @@ function App() {
       <input id="search" type="text" />
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
